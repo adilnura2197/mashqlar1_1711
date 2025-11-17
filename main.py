a183e7171bf6603a842962fc7615c08c20d6a7fa
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2025-11-17 15:23:41 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,78 @@
+#1-misol
+s = "abcdefghij"
+print(s)
+
+toza = s[2:5:1]
+print(toza)
+
+
+#2-misol
+s = "HelloWorld"
+print(s)
+
+toza = s[:5]
+print(toza)
+
+
+#3-misol
+s = "PhytonProgramming"
+print(s)
+
+toza = s[-7:]
+print(toza)
+
+
+#4-misol
+s = "abcdefghijkl"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+
+#5-misol
+s = "0123456789"
+print(s)
+
+toza = s[:9:2]
+print(toza)
+
+
+#6-misol
+s = "WelcomeToPython"
+print(s)
+
+toza = s[3:9]
+print(toza)
+
+
+#7-misol
+s = "PythonLanguage"
+print(s)
+
+toza = s[3:10]
+print(toza)
+
+
+#8-misol
+s = "ABCDEFGHIJKLM"
+print(s)
+
+toza = s[5:10]
+print(toza)
+
+
+#9-misol
+s = "superprogrammer"
+print(s)
+
+toza = s[-4:]
+print(toza)
+
+
+#10-misol
+s = "0123456789"
+print(s)
+
+toza = s[::2]
+print(toza)
